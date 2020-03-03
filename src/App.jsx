523cdd61d42aa73d7cbc6514deb1625f609c925a
abdella206 <f.abdella1@yahoo.com>
2020-03-03 06:56:25 -0800

    updated resume link

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-
+import Particles from 'react-particles-js';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
@@ -46,6 +46,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 50
+	        },
+	        "size": {
+	            "value": 3
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+	}} />
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
